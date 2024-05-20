4969a6671b3bfc65b26cec667b5b51d3d1085899
developerHub01 <developerhubytchannel@gmail.com>
2024-05-20 21:26:54 +0600

    product and interface created

---
+++ src/app/modules/product/product.interface.ts
@@ -0,0 +1,23 @@
+export interface TVariant {
+  types: string;
+  value: string;
+}
+export interface TInventory {
+  quantity: number;
+  inStock: boolean;
+}
+export interface TProduct {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: Array<string>;
+  variants: Array<TVariant>;
+  inventory: TInventory;
+}
+export interface TOrder {
+  email: string;
+  productId: string;
+  price: number;
+  quantity: number;
+}
